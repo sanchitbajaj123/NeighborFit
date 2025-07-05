02057e55509dba1758bc5a574f29d885a239bf4f
sanchit bajaj <sanchitbajaj2003@gmail.com>
2025-07-05 21:48:45 +0530

    alldone

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Analytics } from "@vercel/analytics/react"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
   <React.StrictMode>
+    <Analytics/>
     <App />
   </React.StrictMode>
   </BrowserRouter>
